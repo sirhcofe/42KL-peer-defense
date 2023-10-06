91eb6b1ed1f23bcb2141725f6f99650fa500faf4
Jason <jasonkwm13@gmail.com>
2023-10-06 19:15:44 +0800

    chore: add type for configs

--- src/uiConfig/intraConfig.ts
+++ src/uiConfig/intraConfig.ts
@@ -1,17 +1,19 @@
 
 
+type urlConfigKeys = 'authorizeURL' | 'tokenURL' | 'meURL';
 
-const urlConfig = {
+export const urlConfig: Record<urlConfigKeys, string> = {
   authorizeURL:"https://api.intra.42.fr/oauth/authorize",
   tokenURL:"https://api.intra.42.fr/oauth/token",
   meURL: "https://api.intra.42.fr/v2/me",
-}
+};
 
+type envConfigKeys = 'client_id' | 'client_secret' | 'grant_type' | 'redirect_uri';
 
-
-const envConfig = {
+export const envConfig: Record<envConfigKeys, string> = {
   client_id:process.env.PERR_DEFENSE_CLIENT_ID || "",
   client_secret:process.env.PEER_DEFENSE_CLIENT_SECRET || "",
   grant_type:process.env.PEER_DEFENSE_GRANT_TYPE || "",
   redirect_uri:process.env.PEER_DEFENSE_REDIRECT_URI || "",
-}
+};
+
