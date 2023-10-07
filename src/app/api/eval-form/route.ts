15676a78837891924b7c204c2b06c92edf0560d9
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-10-07 20:43:40 +0800

    feat: include firebase

--- src/app/api/eval-form/route.ts
+++ src/app/api/eval-form/route.ts
@@ -4,7 +4,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 const firestore = getFirestore(app);
 
-export async function GET(req: NextRequest, res: Response) {
+// export async function GET(req: NextRequest, res: NextResponse) {
+//   const param = req.nextUrl.searchParams;
+//   const collectionName = param.get("collectionName");
+//   const snapshot = await getDocs(collection(firestore, collectionName || ""));
+
+//   const data = snapshot.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   }));
+
+//   return Response.json("hello");
+// }
+
+export async function POST(req: NextRequest, res: NextResponse) {
   const param = req.nextUrl.searchParams;
   const collectionName = param.get("collectionName");
   const snapshot = await getDocs(collection(firestore, collectionName || ""));
