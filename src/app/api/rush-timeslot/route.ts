fdd0dc78c45bcfde85e6d31963ce936b5cda72d9
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-10-08 09:38:37 +0800

    feat: add cadet time slot and select time slot api

--- src/app/api/rush-timeslot/route.ts
+++ src/app/api/rush-timeslot/route.ts
@@ -55,27 +55,91 @@ export async function GET(req: NextRequest, res: NextResponse) {
   return Response.json({ data, timeSlots });
 }
 
-type AddTimeslotBody = {
+type SelectTimeslotBody = {
   dateTime: Date;
-  evaluator: string;
   isDefault: boolean;
   teamId: string;
+  customReason: string;
 };
+
+type CadetSlotBody = {
+  evaluator: string;
+  day: number;
+  hour: number;
+  isDefault: boolean;
+};
+
+/**
+ * Handles HTTP POST requests to add a new timeslot document to the specified Firestore collection.
+ * @param req - The Next.js request object.
+ * @param res - The Next.js response object.
+ * @returns A JSON response containing the ID and data of the newly added document, or an error message with a 500 status code if the operation fails.
+ */
+/**
+ * Handles POST requests to add a new timeslot to the "rush-timetables" collection.
+ * @param {NextRequest} req - The Next.js request object.
+ * @param {NextResponse} res - The Next.js response object.
+ * @returns {Promise<void>} - A Promise that resolves when the response is sent.
+ */
 export async function POST(req: NextRequest, res: NextResponse) {
   const collectionName = req.nextUrl.searchParams.get("collection");
-  const data: AddTimeslotBody = await req.json();
 
-  // convert strigify string to Date
-  data.dateTime = new Date(data.dateTime);
-
-  try {
-    const docRef = await addDoc(
-      collection(firestore, collectionName ?? ""),
-      data,
+  let colRef;
+  if (collectionName) colRef = collection(firestore, collectionName);
+  else
+    return Response.json(
+      { error: "Missing param: collection" },
+      { status: 500 },
     );
-    return Response.json({ id: docRef.id, data });
-  } catch (error) {
-    console.error(error);
-    return Response.json({ error: "Failed to add document" }, { status: 500 });
+
+  if (collectionName === "rush-timetables") {
+    const data: SelectTimeslotBody = await req.json();
+
+    if (!data.customReason || !data.dateTime || !data.isDefault || !data.teamId)
+      return Response.json({ error: "Invalid body" }, { status: 500 });
+
+    // convert strigify string to Date
+    data.dateTime = new Date(data.dateTime);
+    try {
+      const docRef = await addDoc(colRef, data);
+      return Response.json({ id: docRef.id, data });
+    } catch (error) {
+      console.error(error);
+      return Response.json(
+        { error: "Failed to add document" },
+        { status: 500 },
+      );
+    }
   }
+
+  if (collectionName === "cadet-slot") {
+    const body: CadetSlotBody = await req.json();
+
+    if (!body.day || !body.hour || !body.evaluator || body.isDefault === null)
+      return Response.json({ error: "Invalid body" }, { status: 500 });
+
+    const dateTime = new Date(2023, 10, body.day, body.hour);
+
+    const data = {
+      dateTime,
+      evaluator: body.evaluator,
+      isDefault: body.isDefault,
+    };
+
+    try {
+      const docRef = await addDoc(colRef, data);
+      return Response.json({
+        message: "Successfully add document",
+        id: docRef.id,
+      });
+    } catch (err) {
+      console.error(err);
+      return Response.json(
+        { error: "Failed to add documemt" },
+        { status: 500 },
+      );
+    }
+  }
+
+  return Response.json({ error: "Invalid collection name" }, { status: 500 });
 }
