00a2b9af65f31e688f50358e51ef9ad26085728f
Zer <livefatezz@gmail.com>
2023-10-08 14:05:36 +0800

    chore: refactor component

--- src/components/timeslot/TimeSlotPicker.tsx
+++ src/components/timeslot/TimeSlotPicker.tsx
@@ -20,6 +20,7 @@ import {
 } from "@chakra-ui/react";
 import CustomTimeModal from "./CustomTimeModal";
 import axios from "axios";
+import PickTime from "./PickTime";
 
 interface ExtCircumModalProps {
   isOpen: boolean;
@@ -129,80 +130,6 @@ function CustomTimeSlots({
           )}
         </>
       )}
-    </div>
-  );
-}
-
-function PickTime({
-  data,
-  student,
-  timeSlots,
-  cadetTimeSlots,
-  selectedDate,
-  setSelectedDate,
-}: {
-  data: any[];
-  student: string;
-  timeSlots: any[];
-  cadetTimeSlots: any[];
-  selectedDate: number;
-  setSelectedDate: Dispatch<SetStateAction<number>>;
-}) {
-  const commonClassNames = (i: number, avail: number) => ({
-    border: i === selectedDate ? "border-[#00B9BB]" : "",
-    text:
-      (i === selectedDate && "text-[#00B9BB]") ||
-      (avail === 0 && "text-gray-500"),
-  });
-
-  return (
-    <div className="flex h-full items-center justify-center">
-      <div className={`grid grid-cols-2 gap-4`}>
-        {student === "pisciner" &&
-          timeSlots.map((slot, i) => {
-            const cNames = commonClassNames(i, slot.availability);
-            return (
-              <button
-                key={i}
-                className={`flex flex-col w-28 h-20 rounded-lg border-2 items-center justify-center ${
-                  slot.availability === 0 && "bg-gray-300 border-gray-300"
-                } ${cNames.border}`}
-                onClick={
-                  slot.availability !== 0
-                    ? () => setSelectedDate(i)
-                    : () => null
-                }
-              >
-                <h3 className={`${cNames.text}`}>{slot.time}</h3>
-                <p className={`text-xs ${cNames.text}`}>
-                  Availability: {slot.availability}
-                </p>
-              </button>
-            );
-          })}
-        {student === "cadet" &&
-          cadetTimeSlots.map((slot, i) => {
-            const cNames = commonClassNames(i, slot.availability);
-            return (
-              <button
-                key={i}
-                className={`flex flex-col w-28 h-20 rounded-lg border-2 items-center justify-center ${
-                  slot.availability === 0 && "bg-gray-300 border-gray-300"
-                } ${cNames.border}`}
-                onClick={
-                  slot.availability !== 0
-                    ? () => setSelectedDate(i)
-                    : () => null
-                }
-              >
-                <h3 className={`${cNames.text}`}>{slot.time}</h3>
-                <p className={`text-xs ${cNames.text}`}>
-                  Availability: {slot.availability}
-                </p>
-              </button>
-            );
-          })}
-      </div>
     </div>
   );
 }
